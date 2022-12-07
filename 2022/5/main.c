7d79390e22ab277de78f20986165b657d5185897
Smino <simon.grundner@gmail.com>
2022-12-08 00:28:45 +0100

    day5

--- 2022/5/main.c
+++ 2022/5/main.c
@@ -6,10 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INPUT_SIZE 80
+#define INPUT_SIZE 40
 #define INPUT_FILE "input.txt"
 #define DELIM_STACK " []"
 #define DELIM_INST " "
+
+// #define DEBUG
 
 // ------------------------------------------------------------
 // UTIL
@@ -38,22 +40,6 @@ typedef struct
 	instruction_t* instructions;
 } cargo_t;
 
-char count_char_in_string(char* str, char c)
-{
-	char count = 0;
-	for (int i = 0; i < strlen(str); i++)
-		count += (str[i] == c);
-	return count;
-}
-
-int stoi(char* str)
-{
-	int result = 0;
-	for (int i = 0; i < strlen(str); i++)
-		result = result * 10 + (str[i] - '0');
-	return result;
-}
-
 void init_cargo(char* filename, cargo_t* cargo)
 {
 	FILE* file = fopen(filename, "r");
@@ -67,6 +53,7 @@ void init_cargo(char* filename, cargo_t* cargo)
 	cargo->instruction_count = 0;
 	cargo->finished_instructions = 0;
 
+	// count max stack height (line count to stack id line)
 	char line[INPUT_SIZE];
 	char ch;
 	while (ch != '1')
@@ -74,10 +61,13 @@ void init_cargo(char* filename, cargo_t* cargo)
 		ch = fgetc(file);
 		cargo->max_stack_height += (ch == '\n');
 	}
-	rewind(file);
+	// got back to the beginning of the stack id line (line length is always at max there)
+	fseek(file, (-2) * sizeof(char), SEEK_CUR);
+	// count max stack count (line length)
 	fgets(line, sizeof(line), file);
 	for (cargo->max_stack_count = 0; line[cargo->max_stack_count] != '\0'; ++cargo->max_stack_count);
 	cargo->max_stack_count /= 4;
+
 	while (fgets(line, sizeof(line), file) != NULL)
 		cargo->instruction_count += (line[0] == 'm');
 	fclose(file);
@@ -87,9 +77,12 @@ void init_cargo(char* filename, cargo_t* cargo)
 
 	for (int i = 0; i < cargo->max_stack_count; i++)
 	{
-		cargo->stacks[i].content = (char*)malloc((cargo->max_stack_height) * sizeof(char));
-		cargo->stacks[i].size = cargo->max_stack_height;
+		cargo->stacks[i].content = (char*)malloc(cargo->max_stack_height * sizeof(char));
+		cargo->stacks[i].size = 0;
 	}
+#ifdef DEBUG
+	printf("max stack height: %d, max stack count: %d, instruction count: %d\n", cargo->max_stack_height, cargo->max_stack_count, cargo->instruction_count);
+#endif
 }
 
 void fill_cargo(char* filename, cargo_t* cargo)
@@ -101,9 +94,41 @@ void fill_cargo(char* filename, cargo_t* cargo)
 		return;
 	}
 
+	// generate stack data struct from file
+	char ch;
+	char spaces = 0;
+	int i = 0;
+	int current_stack_height = cargo->max_stack_height - 1;
+	while (ch = fgetc(file), current_stack_height >= 0)
+	{
+		spaces += (ch == ' ');
+		if (ch == '[')
+		{
+			ch = fgetc(file);
+			cargo->stacks[i].content[current_stack_height] = ch;
+			spaces = 0;
+			cargo->stacks[i].size++;
+			cargo->stacks[i].max_size = cargo->stacks[i].size;
+			i %= cargo->max_stack_height;
+			i++;
+		}
+		else if (spaces >= 4)
+		{
+			cargo->stacks[i].content[current_stack_height] = '\0';
+			i %= cargo->max_stack_height;
+			spaces = 0;
+			i++;
+		}
+		if (ch == '\n')
+		{
+			current_stack_height--;
+			i = 0;
+		}
+	}
+
+	// generate instruction data struct from file
 	instruction_t* first_instruction = cargo->instructions;
 	char line[INPUT_SIZE];
-	int current_stack_height = cargo->max_stack_height - 1;
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
 		if (line[0] == 'm')
@@ -121,16 +146,7 @@ void fill_cargo(char* filename, cargo_t* cargo)
 		}
 	}
 
-	char ch;
-
-	while (ch = fgetc(file), ch != '1')
-	{
-		// if ch = [ then fgetc = value
-		// count spaces, if 4 spaces in row ten value= '#'
-
-	}
-
-	cargo->instructions = first_instruction;
+	cargo->instructions = first_instruction; // reset pointer to first instruction
 	fclose(file);
 }
 
@@ -142,54 +158,150 @@ void free_cargo(cargo_t* cargo)
 	free(cargo->instructions);
 }
 
-// recursive function to reorganize items in stack according to instruction
-void restack_cargo(cargo_t* cargo)
+void restack_cargo1(cargo_t* cargo)
 {
+#ifdef DEBUG
+	if (cargo->finished_instructions >= 10)
+#else
+	// return if all instructions are finished
 	if (cargo->finished_instructions >= cargo->instruction_count)
+#endif
 	{
+		// realloc according to final size
 		for (int i = 0; i < cargo->max_stack_count; i++)
-			printf("%s [%d]\n", cargo->stacks[i].content, cargo->stacks[i].size);
+			cargo->stacks[i].content = (char*)realloc(cargo->stacks[i].content, cargo->stacks[i].size * sizeof(char));
 		return;
 	}
-	int src_stack_id = cargo->instructions[cargo->finished_instructions].src_stack_id;
-	int dest_stack_id = cargo->instructions[cargo->finished_instructions].dest_stack_id;
+
+	int src_stack_id = cargo->instructions[cargo->finished_instructions].src_stack_id - 1;
+	int dest_stack_id = cargo->instructions[cargo->finished_instructions].dest_stack_id - 1;
 	int items_to_move = cargo->instructions[cargo->finished_instructions].items_to_move;
 
-	printf("Instruction %3d: Moving %3d items from stack %d(%3d|%3d) to stack %d(%3d|%3d) -> Max: %2d\n",
-		cargo->finished_instructions + 1,
+#ifdef DEBUG
+	int src_stack_size = cargo->stacks[src_stack_id].size; // dont use this, use cargo->stacks[src_stack_id].size instead
+	int dest_stack_size = cargo->stacks[dest_stack_id].size; // dont use this, use cargo->stacks[dest_stack_id].size instead
+
+	printf("Instruction %3d: Moving %2d items from stack %d (%2d|%2d|%2d) to stack %d (%2d|%2d|%2d)\n",
+		cargo->finished_instructions,
 		cargo->instructions[cargo->finished_instructions].items_to_move,
-		cargo->instructions[cargo->finished_instructions].src_stack_id,
+		src_stack_id,
 		cargo->stacks[src_stack_id].size,
 		cargo->stacks[src_stack_id].size - items_to_move,
-		cargo->instructions[cargo->finished_instructions].dest_stack_id,
+		cargo->stacks[src_stack_id].max_size,
+		dest_stack_id,
 		cargo->stacks[dest_stack_id].size,
-		cargo->stacks[dest_stack_id].size - items_to_move,
+		cargo->stacks[dest_stack_id].size + items_to_move,
+		cargo->stacks[dest_stack_id].max_size);
+#endif
+	// realloc destination stack if needed
+	if (cargo->stacks[dest_stack_id].size + items_to_move > cargo->stacks[dest_stack_id].max_size)
+	{
+#ifdef DEBUG
+		printf("---------- Reallocated %d for stack %d ----------\n",
+			cargo->stacks[dest_stack_id].size + items_to_move, dest_stack_id);
+#endif
+		cargo->stacks[dest_stack_id].content = realloc(cargo->stacks[dest_stack_id].content, (cargo->stacks[dest_stack_id].size + 1 + items_to_move) * sizeof(char));
+		cargo->stacks[dest_stack_id].max_size = cargo->stacks[dest_stack_id].size + items_to_move;
+	}
+	// concat item by item to destination stack and trim from source stack
+	for (int i = 0; i < items_to_move; i++)
+	{
+		cargo->stacks[dest_stack_id].size++;
+		strcat(cargo->stacks[dest_stack_id].content, &cargo->stacks[src_stack_id].content[cargo->stacks[src_stack_id].size - 1]);
+		cargo->stacks[src_stack_id].size--;
+		cargo->stacks[src_stack_id].content[cargo->stacks[src_stack_id].size] = '\0';
+	}
 
-		cargo->max_stack_height);
+#ifdef DEBUG
+	if (cargo->stacks[src_stack_id].size < 0)
+	{
+		printf("Error: stack %d is empty (%d)\n", src_stack_id, cargo->stacks[src_stack_id].size);
+		return;
+	}
+	for (int i = 0; i < cargo->max_stack_count; i++)
+		printf("[id: %d] [sz: %2d]: %s\n", i, cargo->stacks[i].size, cargo->stacks[i].content);
+	printf("\n");
+#endif
 
-	cargo->stacks[dest_stack_id].size += items_to_move;
-	cargo->stacks[src_stack_id].size -= items_to_move;
-
-	// if (cargo->stacks[dest_stack_id].size > cargo->stacks[dest_stack_id].max_size)
-	// {
-	// 	if (!realloc(cargo->stacks[dest_stack_id].content, cargo->stacks[dest_stack_id].size * sizeof(char)))
-	// 	{
-	// 		printf("Error reallocating memory for stack %d", dest_stack_id);
-	// 		return;
-	// 	}
-	// 	printf("---------- Reallocated %d for stack %d ----------\n",
-	// 		cargo->stacks[dest_stack_id].size, dest_stack_id);
-	// 	cargo->stacks[dest_stack_id].max_size = cargo->stacks[dest_stack_id].size;
-	// }
-	// if (cargo->stacks[src_stack_id].size < 0)
-	// {
-	// 	printf("Error: stack %d is empty (%d)\n", src_stack_id, cargo->stacks[src_stack_id].size);
-	// 	// return;
-	// }
-	cargo->finished_instructions++;
-	return restack_cargo(cargo);
+	cargo->finished_instructions++; // goto next instruction
+	return restack_cargo1(cargo); // recursive call
 }
 
+void restack_cargo2(cargo_t* cargo)
+{
+#ifdef DEBUG
+	if (cargo->finished_instructions >= 10)
+#else
+	// return if all instructions are finished
+	if (cargo->finished_instructions >= cargo->instruction_count)
+#endif
+	{
+		// realloc according to final size
+		for (int i = 0; i < cargo->max_stack_count; i++)
+			cargo->stacks[i].content = (char*)realloc(cargo->stacks[i].content, cargo->stacks[i].size * sizeof(char));
+		return;
+	}
+
+	int src_stack_id = cargo->instructions[cargo->finished_instructions].src_stack_id - 1;
+	int dest_stack_id = cargo->instructions[cargo->finished_instructions].dest_stack_id - 1;
+	int items_to_move = cargo->instructions[cargo->finished_instructions].items_to_move;
+
+#ifdef DEBUG
+	int src_stack_size = cargo->stacks[src_stack_id].size; // dont use this, use cargo->stacks[src_stack_id].size instead
+	int dest_stack_size = cargo->stacks[dest_stack_id].size; // dont use this, use cargo->stacks[dest_stack_id].size instead
+
+	printf("Instruction %3d: Moving %2d items from stack %d (prev: %2d|cur: %2d|max: %2d) to stack %d (%2d|%2d|%2d)\n",
+		cargo->finished_instructions,
+		cargo->instructions[cargo->finished_instructions].items_to_move,
+		src_stack_id,
+		cargo->stacks[src_stack_id].size,
+		cargo->stacks[src_stack_id].size - items_to_move,
+		cargo->stacks[src_stack_id].max_size,
+		dest_stack_id,
+		cargo->stacks[dest_stack_id].size,
+		cargo->stacks[dest_stack_id].size + items_to_move,
+		cargo->stacks[dest_stack_id].max_size);
+#endif
+
+	// realloc destination stack if needed
+	if (cargo->stacks[dest_stack_id].size + items_to_move > cargo->stacks[dest_stack_id].max_size)
+	{
+#ifdef DEBUG
+		printf("---------- Reallocated %d for stack %d ----------\n",
+			cargo->stacks[dest_stack_id].size + items_to_move, dest_stack_id);
+#endif
+		cargo->stacks[dest_stack_id].content = realloc(cargo->stacks[dest_stack_id].content, (cargo->stacks[dest_stack_id].size + 1 + items_to_move) * sizeof(char));
+		cargo->stacks[dest_stack_id].max_size = cargo->stacks[dest_stack_id].size + items_to_move;
+	}
+
+	// concat whole item section to destination stack and trim from source stack
+	cargo->stacks[dest_stack_id].size += items_to_move;
+	strcat(cargo->stacks[dest_stack_id].content,
+		&cargo->stacks[src_stack_id].content[cargo->stacks[src_stack_id].size - items_to_move]);
+	cargo->stacks[src_stack_id].size -= items_to_move;
+	cargo->stacks[src_stack_id].content[cargo->stacks[src_stack_id].size] = '\0';
+
+#ifdef DEBUG
+	if (cargo->stacks[src_stack_id].size < 0)
+	{
+		printf("Error: stack %d is empty (%d)\n", src_stack_id, cargo->stacks[src_stack_id].size);
+		return;
+	}
+	for (int i = 0; i < cargo->max_stack_count; i++)
+		printf("[id: %d] [sz: %2d]: %s\n", i, cargo->stacks[i].size, cargo->stacks[i].content);
+	printf("\n");
+#endif
+	cargo->finished_instructions++; // goto next instruction
+	return restack_cargo2(cargo); // recursive call
+}
+
+char* top_of_each_stack(cargo_t* cargo)
+{
+	char* top_of_each;
+	for (int i = 0; i < cargo->max_stack_count; i++)
+		top_of_each[i] = cargo->stacks[i].content[cargo->stacks[i].size - 1];
+	return top_of_each;
+}
 
 // ------------------------------------------------------------
 // MAIN
@@ -200,8 +312,9 @@ int main(int argc, char const* argv[])
 	cargo_t cargo;
 	init_cargo(INPUT_FILE, &cargo);
 	fill_cargo(INPUT_FILE, &cargo);
-	printf("max stack height: %d, max stack count: %d, instruction count: %d\n", cargo.max_stack_height, cargo.max_stack_count, cargo.instruction_count);
-	// restack_cargo(&cargo);
+	// restack_cargo1(&cargo);
+	// restack_cargo2(&cargo);
+	printf("%s\n", top_of_each_stack(&cargo));
 	free_cargo(&cargo);
 	return 0;
 }
